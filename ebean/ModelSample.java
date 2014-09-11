c559673db173bb236f6f5d2687622ee00c047a26
os <ogasawaraShinnosuke@users.noreply.github.com>
2014-09-11 17:33:02 +0900

    Update ModelSample.java
    memo expressionList

--- ebean/ModelSample.java
+++ ebean/ModelSample.java
@@ -1,2 +1,26 @@
 class ModelSample {
+  
+  
+      public static List<Sample> findBySample() {
+        Finder<Long, Sample> finder = selectFinder(dbResouceType);
+        return new Model.Finder<Long, Sample>("MASTER", Long.class,
+                Sample.class).where().where()
+                
+                // OR条件
+                .disjunction()
+                
+                  // AND条件
+                  .conjunction().ne("id", 1).ne("id", 2)
+
+                  .ne("name", "gomi")
+                  .eq("date", "2013-01-01")
+                  .eq("created", "2013-02-09")
+
+                .endJunction()
+  
+                .eq("flg1", true)
+                .eq("flg2", false)
+                
+                .findList();
+    }
 }
